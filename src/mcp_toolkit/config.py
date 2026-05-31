5a8674cc7b4e3d26d0dd644d093f81fa597ffa0a
sarmakska <60353896+sarmakska@users.noreply.github.com>
2026-05-31 18:27:17 +0100

    refactor(config): use SettingsConfigDict, drop deprecated class Config

--- src/mcp_toolkit/config.py
+++ src/mcp_toolkit/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -12,6 +12,4 @@ class Settings(BaseSettings):
 
     otel_endpoint: str = ""
 
-    class Config:
-        env_prefix = "MCP_"
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_prefix="MCP_", env_file=".env")
